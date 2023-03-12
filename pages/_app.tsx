594ea537761f1db7f4a95271ab118d2986d5fde4
ehtsham623 <ehtshamulhaqat@gmail.com>
2023-03-13 02:51:12 +0500

    merge

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,28 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    return () => {};
+  }, []);
+
   return (
     <>
       <Script
         src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r68/three.min.js"
-        strategy="lazyOnload"
-        onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
-        }
+        // strategy="lazyOnload"
+        // onLoad={() =>
+        //   console.log(`script loaded correctly, window.FB has been populated`)
+        // }
       />
       <Script
         src="script.js"
-        strategy="lazyOnload"
-        onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
-        }
+        // strategy="lazyOnload"
+        // onLoad={() =>
+        //   console.log(`script loaded correctly, window.FB has been populated`)
+        // }
       />
       <div className="" data-theme="green">
         <Component {...pageProps} />
