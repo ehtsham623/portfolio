0e94c59ecf12d64dda44d45f174cbf62ea764acf
ehtsham623 <ehtshamulhaqat@gmail.com>
2023-06-05 03:14:24 +0500

    added image optimization by adding next image

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
+
 function About() {
   return (
     <>
       <div className="flex flex-row h-full justify-center items-center ">
-        <img className="w-80 h-80 rounded-lg" src="/eht.png" />
+        <Image
+          src={"/eht.png"}
+          alt="Picture of the author"
+          className=" rounded-lg"
+          width={320}
+          height={320}
+        />
         <div>
           <div>ehtsham ul haq</div>
           <div>software engineer</div>
