e5ed92293f235ad65e08c1c598cda965481aa4b6
ehtsham623 <ehtshamulhaqat@gmail.com>
2023-06-13 17:43:45 +0500

    removed env from git

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Portfolio() {
-  return <div>portfolio</div>;
+  return (
+    <div className="flex h-full w-full mt-96 justify-center items-center">
+      <div className="flex flex-col w-full md:w-8/12 h-auto mx-5 mt-24 md:mt-0 justify-start sm:justify-center items-start">
+        <div className="text-3xl font-bold text-white">
+          My <span className="text-primary"> Resume</span>
+        </div>
+        <span className="w-full border border-t-[1.6px] border-b-0 mt-5 mb-12 border-spacing-8 border-background border-dashed" />
+    
+      </div>
+    </div>
+  );
 }
 
 export default Portfolio;
