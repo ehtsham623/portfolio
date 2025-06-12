31e64f7177f549d2f0d7a1f6a49164189275a244
Ehtsham <ehtshamulhaqat@gmail.com>
2025-06-12 17:58:54 +0500

    nav bug fix

--- layouts/mainLayout.tsx
+++ layouts/mainLayout.tsx
@@ -8,7 +8,8 @@ export default function MainLayout({ children }: any) {
       <div className="relative ">
         <Settings />
         <div className="flex flex-col h-screen  top-0 bottom-0 ">
-          <div className="sticky z-50 top-0 bottom-0 ">
+          <div className=" top-0 bottom-0 ">
+            {/* sticky z-50 */}
             <Header />
           </div>
           <main className="flex flex-1 h-full w-full flex-col text-primary">
